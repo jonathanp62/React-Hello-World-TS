a650c5375d1fd6ec81b0b36591c936c56ef6f96e
Jonathan Parker <jonathanp62@gmail.com>
2025-10-17 06:42:40 -0400

    Initial 0.1.0 commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+/*
+ * (#)App.tsx   0.1.0   10/17/2025
+ *
+ * @author  Jonathan Parker
+ * @version 0.1.0
+ * @since   0.1.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import './styles/styles.css'
+import type { JSX } from "react";
+import { Config } from "./Config.tsx"
+import Header from "./Header.tsx";
+import Paragraph from "./Paragraph.tsx";
+import packageJson from "../package.json";
+
+/**
+ * The App component.
+ *
+ * @returns {JSX.Element}
+ */
+function App(): JSX.Element {
+    const greeting: string = "Hello, " + Config.author + "!";
+    const textContent: string = "This isn't that much but it is a nice start!";
+    const appName: string = packageJson.name;
+
+    return (
+      <>
+          <Header greeting={greeting} />
+          <Paragraph message={textContent} version={packageJson.version} />
+          <p className="p-text">The {appName} application realized in Typescript.</p>
+      </>
+  );
+}
+
+export default App;
